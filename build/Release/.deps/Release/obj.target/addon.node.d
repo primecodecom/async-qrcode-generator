461df8804f99b0f0dfcc9449b383769a636d1b4f
Alexander Makedonski <alexanderm@primecode.company>
2020-08-26 09:28:22 +0300

    add .gitingore

--- build/Release/.deps/Release/obj.target/addon.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/addon.node := g++ -shared -pthread -rdynamic -m64  -Wl,-soname=addon.node -o Release/obj.target/addon.node -Wl,--start-group Release/obj.target/addon/addon.o Release/obj.target/addon/DataProcessingAsyncWorker.o Release/obj.target/addon/QrCode.o Release/obj.target/node_modules/node-addon-api/nothing.a -Wl,--end-group 
